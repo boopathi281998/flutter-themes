9465156a051ed10a2d3f75f52efa72a532d1e274
boopathi281998 <nkboopathi1998@gmail.com>
2024-11-14 10:39:05 +0530

    Update light.theme.dart

--- lib/themes/light.theme.dart
+++ lib/themes/light.theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const brightness = Brightness.light;
 const primaryColor = const Color(0xFF00C569);
-const lightColor = const Color(0xFFFFFFFF);
+const lightColor = const Color(0xFF1f048c);
 const backgroundColor = const Color(0xFFF5F5F5);
 
 ThemeData lightTheme() {
